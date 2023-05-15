71e47ce8d5d15e93ce053a9ce5a7129330df3bb5
Renan Moreira <renan.tsx@gmail.com>
2023-05-15 10:58:14 -0300

    Install axios + context resolvers

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -9,33 +9,9 @@ const user = () => {
   };
 };
 
-const users = () => {
-  return [
-    {
-      id: '101',
-      indexRef: 1,
-      firtName: 'Pedro',
-      lastName: 'de Souza',
-      createdAt: '2023-05-12T18:44:30.446Z',
-      updatedAt: '2023-05-12T18:44:30.446Z',
-    },
-    {
-      id: '102',
-      indexRef: 2,
-      firtName: 'João',
-      lastName: 'Moreira',
-      createdAt: '2023-05-12T18:45:59.949Z',
-      updatedAt: '2023-05-12T18:45:59.949Z',
-    },
-    {
-      id: '103',
-      indexRef: 3,
-      firtName: 'Maria',
-      lastName: 'Pimentel',
-      createdAt: '2023-05-12T18:46:40.957Z',
-      updatedAt: '2023-05-12T18:46:40.957Z',
-    },
-  ];
+const users = async (_obj, _arg, context, _info) => {
+  const users = await context.axios.get('http://localhost:3000/users');
+  return users.data;
 };
 
 export const userResolvers = {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { ApolloServer } from 'apollo-server';
+import axios from 'axios';
 import { resolvers, typeDefs } from './graphql';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: () => {
+    return {
+      axios,
+    };
+  },
 });
 
 server.listen(4003).then(({ url }) => {
